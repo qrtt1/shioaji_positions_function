6e03214d42387a6f63973a1aeaf19f84e0e16232
Ching Yi, Chan <chingyichan.tw@gmail.com>
2025-10-17 14:37:20 +0800

    chore: upgrade Lambda runtime to Python 3.12
    Upgrade from Python 3.10 to 3.12 to resolve GLIBC compatibility issues
    with the pynacl library. Python 3.12 runtime uses Amazon Linux 2023
    which includes GLIBC 2.34+, meeting the requirements of shioaji dependencies.
    
    Changes:
    - Update GitHub Actions workflow to use Python 3.12
    - Update Lambda function creation/update script to use python3.12 runtime
    - Add Python 3.12 to setup.py classifiers

--- scripts/create-or-update-lambda-function.sh
+++ scripts/create-or-update-lambda-function.sh
@@ -4,7 +4,7 @@ then
   echo "Lambda function does not exist, creating..."
   aws lambda create-function \
     --function-name $TARGET_LAMBDA_FUNCTION \
-    --runtime python3.10 \
+    --runtime python3.12 \
     --timeout 30 \
     --handler $TARGET_LAMBDA_FUNCTION_HANDLER \
     --code "S3Bucket=$TARGET_S3_BUCKET,S3Key=$TARGET_S3_KEY" \
@@ -13,4 +13,5 @@ then
 else
   echo "Lambda function already exists, updating"
   aws lambda update-function-code --function-name ${TARGET_LAMBDA_FUNCTION} --s3-bucket ${TARGET_S3_BUCKET} --s3-key ${TARGET_S3_KEY}
+  aws lambda update-function-configuration --function-name ${TARGET_LAMBDA_FUNCTION} --runtime python3.12
 fi
--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
